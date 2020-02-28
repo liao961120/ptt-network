ebe4fd9c1ab5fcbcfe4d43c4af7c633676389a01
liao <liao961120@gmail.com>
2020-02-28 15:17:10 +0800

    JSON format IO for Node

--- graph.py
+++ graph.py
@@ -1,6 +1,7 @@
 #%%
 import datetime
 import jieba
+import json
 import networkx as nx
 
 class Graph(nx.Graph):
@@ -17,14 +18,35 @@ class Graph(nx.Graph):
 
 class Node():
 
-    def __init__(self, id_):
-        self.id = id_
-        self.corpus = {}
-        self.vocab = {}
+    def __init__(self, id_: [str, int]=None, fromJson: str=None):
+        if id_ is None and fromJson is None:
+            raise Exception("Need either id_ or fromJson")
+
+        if fromJson is not None:
+            self.__fromJson(fromJson)
+        else:
+            self.id = id_
+            self.corpus = {}
+            self.vocab = {}
 
     def __repr__(self):
         return f"<Node, node_id: {self.id}>"
 
+
+    def __fromJson(self, json_str: str):
+        node = json.loads(json_str)
+        self.id = node['id']
+        self.corpus = node['corpus']
+        self.vocab = node['vocab']
+
+
+    def toJson(self):
+        return json.dumps({
+            "id": self.id,
+            "corpus": self.corpus,
+            "vocab": self.vocab
+        }, ensure_ascii=False)
+    
 
     def add_comment(self, date, content, type="pos"):
         """Add comment to corpus
@@ -53,14 +75,14 @@ class Node():
         ----------------
 
         {
-            2019-01-20: [
+            '2019-01-20': [
                 {   
                     type: "pos",
                     content: "segmented string"
                 },
                 {...}
             ],
-            2020-01-20: [...],
+            '2020-01-20': [...],
             ...
         }
         '''
@@ -68,30 +90,30 @@ class Node():
 
     def getVocab(self, start="1900-01-01", end="2050-12-31", force=False):
         
-        if force or self.vocab.get((start, end)) is None:
+        if force or self.vocab.get(f"{start}_{end}") is None:
 
-            start = datetime.date.fromisoformat(start)
-            end = datetime.date.fromisoformat(end)
+            start_d = datetime.date.fromisoformat(start)
+            end_d = datetime.date.fromisoformat(end)
 
             raw_content = ''
             for k, cmts in self.corpus.items():
                 # Get all comments of specific date
                 dt = datetime.date.fromisoformat(k)
-                if start <= dt and dt <= end:
+                if start_d <= dt and dt <= end_d:
                     raw_content += '\n'.join(c['content'] for c in cmts)
 
             # Renew comment
-            self.vocab[(start, end)] = set(raw_content.split())
+            self.vocab[f"{start}_{end}"] = list(set(raw_content.split()))
         
         '''
         Vocab Structure
         ---------------
 
         {
-            (start_date, end_date) : {'word1', 'word2', ...},
+            '1900-01-01_2050-12-31' : ['word1', 'word2', ...],
             ...
         }
         '''
-        return self.vocab[(start, end)]
+        return self.vocab[f"{start}_{end}"]
 
 
